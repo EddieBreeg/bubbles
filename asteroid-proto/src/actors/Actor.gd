743ef7cfb5ed3f96d1c3eee7e7f3856ba27bb416
Aymeric Ferron <aymericferron@aol.com>
2021-07-10 14:16:45 +0200

    feat(planet): add planet

--- asteroid-proto/src/actors/Actor.gd
+++ asteroid-proto/src/actors/Actor.gd
@@ -12,6 +12,12 @@ var hp = 3
 func _ready():
 	pass # Replace with function body.
 
+func _on_BulletDetector_body_entered(body):
+	_take_damage(1)
+
+
+func _take_damage(n: int) -> void:
+	hp -= n
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
