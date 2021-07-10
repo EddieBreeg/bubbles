6263f0fdad02a94276d0f7857d2e76ae0da580c4
Aymeric Ferron <aymericferron@aol.com>
2021-07-10 15:26:27 +0200

    refactor(sprites): planet and player

--- asteroid-proto/src/actors/Enemy.gd
+++ asteroid-proto/src/actors/Enemy.gd
@@ -2,14 +2,21 @@ extends "res://src/actors/Actor.gd"
 class_name Enemy
 
 # Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+var shoot_rock = preload('..//projectiles/BulletStone.tscn')
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
+func shot_flame():
+	var shoot_instance = shoot_rock.instance()
+	shoot_instance.position = get_global_position()
+	shoot_instance.velocity = velocity
+	get_parent().add_child(shoot_instance)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
+func _process(delta: float) -> void:
+	shot_flame()
 
