1e4c307aeddbfdbc72f5105b96d85d7b39d9ad29
Eddie Breeg <eddiebreeg0@protonmail.com>
2021-07-10 21:56:31 +0200

    added GPS

--- asteroid-proto/src/actors/Motherland.gd
+++ asteroid-proto/src/actors/Motherland.gd
@@ -9,6 +9,8 @@ extends Planet
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+func _take_damage(n: int) -> void:
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
