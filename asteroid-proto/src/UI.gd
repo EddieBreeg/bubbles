b7fe4f1cc9db1987506637acbd95a95e8aa50454
Eddie Breeg <eddiebreeg0@protonmail.com>
2021-07-10 20:22:13 +0200

    red text when in danger

--- asteroid-proto/src/UI.gd
+++ asteroid-proto/src/UI.gd
@@ -11,6 +11,7 @@ func _process(_delta):
 	var player = get_parent()
 	var rocks = player._get_rock_count()
 	get_node('RocksLabel').text = str(rocks)
+	get_node('RocksLabel').set("custom_colors/font_color", Color(1,0,0) if rocks<=3 else Color(1,1,1))
 	var lives = player._get_life_count()
 	get_node('LivesLabel').text = str(lives)
 	var score = player._get_score()
