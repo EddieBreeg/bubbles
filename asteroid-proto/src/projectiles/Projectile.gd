86aa960d5846e96ee24a104e2147c6ff760bbdab
Aymeric Ferron <aymericferron@aol.com>
2021-07-10 15:00:56 +0200

    refactor(life_bullet): hitbox

--- asteroid-proto/src/projectiles/Projectile.gd
+++ asteroid-proto/src/projectiles/Projectile.gd
@@ -12,7 +12,6 @@ signal hit
 # Declare member variables here. Examples:
 var life_time = 3
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	velocity += projectile_speed*Vector2(1,0).rotated(rotation)
@@ -32,6 +31,7 @@ func _on_Projectile_body_entered(body: Node) -> void:
 	if body.is_in_group("EnemyGroup"):
 		print("Hit an enemy")
 	emit_signal('hit')
+	
 # func _on_BulletStone_body_entered(body: Node) -> void:
 # 	self.hide()
 # 	if body.is_in_group("EnemyGroup"):
