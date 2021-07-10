2f954003ab7ad157eaf59fe75926bdd212543664
Aymeric Ferron <aymericferron@aol.com>
2021-07-10 18:46:32 +0200

    fix(projectile): enemy does not hurt planet anymore

--- asteroid-proto/src/actors/Planet.gd
+++ asteroid-proto/src/actors/Planet.gd
@@ -1,6 +1,9 @@
 extends Actor
 class_name Planet
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.add_to_group('Planet')
 
 func _on_Planet_die():
 	queue_free()
---
+++ asteroid-proto/src/projectiles/BulletStone.gd
@@ -0,0 +1,24 @@
+extends Projectile
+
+class_name BulletStone 
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Projectile_body_entered(body: Node) -> void:
+	get_node("Hitbox").set_deferred('disabled', true)
+	self.hide()
+	if((body.is_in_group("Planet")) or (body.is_in_group("Enemy"))):
+		body._take_damage(1)
